beb38c111065981dcf7dd2d9fe61a0a405d9e98c
leofusinato <leonardo.alex.fusinato@gmail.com>
2020-09-07 13:57:47 -0300

    Usuário logado na tela

--- FusuanLanchesV2/src/main/java/controller/ControllerLogin.java
+++ FusuanLanchesV2/src/main/java/controller/ControllerLogin.java
@@ -97,12 +97,14 @@ public class ControllerLogin extends Controller {
 
     private Usuario validaLogin(Usuario usuario) {
         Usuario retorno = null;
+        
         for (Usuario usuarioSalvo : ControllerUsuario.getInstance().listar()) {
             if (usuarioSalvo.getLogin().equals(usuario.getLogin()) && usuarioSalvo.getSenha().equals(usuario.getSenha())) {
                 retorno = usuarioSalvo;
                 break;
             }
         } 
+        
         return retorno;
     }
 
